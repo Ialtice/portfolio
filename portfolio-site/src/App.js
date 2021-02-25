6b4e793432eb07590065eb42ab14fa82c9237b4a
Ialtice <48520799+Ialtice@users.noreply.github.com>
2021-02-25 12:33:29 +0100

    first commit

---
+++ portfolio-site/src/App.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import './App.css';
+
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      title: 'Isaac Altice',
+      headerLinks: [
+        { title: 'Home', path: '/' },
+        { title: 'About', path: '/about' },
+        { title: 'Contac', path: '/contact' }
+      ],
+      home: {
+        title: 'Learning To Make A Difference',
+        subTitle: 'Gaining Experience',
+        subText: 'Check out My Projects Below'
+      },
+      about: {
+        title: 'About Me',
+        
+      },
+      contact: {
+        title: 'Im eager to get in contact with you!',
+        
+      },
+    }
+  }
+  render() {
+    return (
+      <Router>
+        <Container className="p-0" fluid={true}>
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
+            <Navbar.Brand>Isaac Altice</Navbar.Brand>
+            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/about">About</Link>
+                <Link className="nav-link" to="/contact">Contact</Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </Container> 
+      </Router>
+    );
+  }
+  
+}
+
+export default App;

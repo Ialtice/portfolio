0858127100a675b1465850918380bff2b183b9ae
Ialtice <48520799+Ialtice@users.noreply.github.com>
2021-02-25 16:43:52 +0100

    Added routing for home,about , contact pages

---
+++ portfolio-site/src/pages/AboutPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function AboutPage(props){
+    return(
+        <p>AboutPage Working</p>
+    );
+}
+
+export default AboutPage;
---
+++ portfolio-site/src/pages/ContactPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ContactPage(props){
+    return(
+        <p>ContactPage Working</p>
+    );
+}
+
+export default ContactPage;

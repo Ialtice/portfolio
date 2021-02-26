cc5aece45adc99547c5628f6f12a359d40cbb2bb
Ialtice <48520799+Ialtice@users.noreply.github.com>
2021-02-26 18:14:19 +0100

    Added heading to HomePage

--- portfolio-site/src/pages/HomePage.js
+++ portfolio-site/src/pages/HomePage.js
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import Heading from '../components/Heading'
 function HomePage(props){
     return(
-        <p>HomePage Working</p>
+        <Heading title={props.title} subTitle={props.subTitle} subText={props.subText} />
     );
 }
 
